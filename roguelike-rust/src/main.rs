7305bc95cabfb5377da64c3b72fa01e4b2a45d05
c0d3b34r <c0d3.3r4b34r@pm.me>
2022-11-04 20:25:09 +0100

    2.3. Walking a map

--- roguelike-rust/src/main.rs
+++ roguelike-rust/src/main.rs
@@ -1,7 +1,7 @@
 use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
-use specs_derive::Component;
+use specs_derive::*;
 
 #[derive(Component)]
 struct Position {
@@ -22,17 +22,56 @@ struct LeftMover{}
 #[derive(Component, Debug)]
 struct Player {}
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
+
 struct State {
     ecs: World,
+}
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
 }
 
 fn try_move_player(delta_x: i32, delta_y:i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79 , max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79 , max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -57,6 +96,9 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+        
         let positions = self.ecs.read_storage::<Position>();
         let rendereables = self.ecs.read_storage::<Renderable>();
 
@@ -89,6 +131,28 @@ impl State {
     }
 }
 
+fn draw_map(map: &[TileType], ctx : &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter(){
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5),RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0),RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+
 fn main()->rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -102,6 +166,8 @@ fn main()->rltk::BError {
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
     
+    gs.ecs.insert(new_map());
+
 
     gs.ecs
         .create_entity()
